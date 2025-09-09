56af24503bc34c5acf7fb57c144a323843d76ca0
”ns-develops” <test.tt9061030@gmail.com>
2025-09-09 13:09:59 +0200

    continueing with three.js bulb

---
+++ src/types/three.examples.d.ts
@@ -0,0 +1,15 @@
+declare module "three/examples/jsm/loaders/GLTFLoader" {
+    import { Loader, LoadingManager, Group } from "three";
+  
+    export class GLTFLoader extends Loader {
+      constructor(manager?: LoadingManager);
+      load(
+        url: string,
+        onLoad: (gltf: { scene: Group }) => void,
+        onProgress?: (event: ProgressEvent) => void,
+        onError?: (event: ErrorEvent) => void
+      ): void;
+    }
+  }
+
+  // implementing lighting bulb
